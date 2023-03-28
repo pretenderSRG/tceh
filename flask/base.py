77e7c2e18722c24bc2083f1625c42cb1f75c7a40
Serg Pretender <sergdynamite@gmail.com>
2023-03-28 10:37:06 +0300

    add forms

--- flask/base.py
+++ flask/base.py
@@ -3,9 +3,28 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return "hello world"
+@app.route('/hello/<user>')
+def home(user):
+    return "hello user: " + user
+
+
+@app.route('/add/<int:x>/<int:y>')
+def add(x, y):
+    return "sun of two numbers = " + str(x + y)
+
+
+@app.route('/len/<s1>/<s2>/<s3>')
+def mx_len(s1, s2, s3):
+    max_len = sorted([s1, s2, s3], key=len, reverse=True)
+    return "Max leb str is = " + max_len[0]
+
+
+@app.route('/<path:pt>')
+def path_exists(pt):
+    import os
+    check_path = os.path.exists(pt)
+    # return "yes" if check_path else "no"
+    return os.listdir(pt)
 
 
 if __name__ == '__main__':
